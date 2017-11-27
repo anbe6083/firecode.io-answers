23183433e5c5bd9b5286ba470f5c253dd0eef2e6
anbe6083 <anbe6083@colorado.edu>
2017-11-27 15:21:17 -0800

    Added edge cases to comments

--- Find the Nth Node from the end without using extra memory - Linked List.java
+++ Find the Nth Node from the end without using extra memory - Linked List.java
@@ -1,3 +1,8 @@
+/*
+Edge cases to take into account here:
+    * What if the head node is null?
+    * What is n is greater than the length of the list? What if it's negative?
+*/
 public ListNode findNthNodeFromEnd(ListNode head, int n) {
     if(head == null) return null;
     int count = 0, length = 0, target = 0;
