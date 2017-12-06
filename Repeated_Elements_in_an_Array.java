cc64c3042e1c17c4185a6868d7df32d528098a66
anbe6083 <anbe6083@colorado.edu>
2017-12-06 13:17:26 -0800

    Added Repeated Elements in an Array problem

---
+++ Repeated_Elements_in_an_Array.java
@@ -0,0 +1,13 @@
+public static String duplicate(int[] numbers){
+    if(numbers == null) return null;
+    TreeSet<Integer> ts = new TreeSet<>();
+    HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+    for(int i = 0; i < numbers.length; i++) {
+        if(!hm.containsKey(numbers[i])  ) {
+            hm.put( numbers[i], 1 );
+        } else {
+            ts.add(numbers[i]);
+        }
+    }
+    return ts.toString();
+}
